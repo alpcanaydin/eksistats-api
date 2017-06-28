5e3f82bba1077b2b6aa7de4862d77097dad0ff3d
alpcanaydin <alpcan@alpcanaydin.com>
2017-06-28 16:31:29 +0300

    Es search lib added.

--- controller/home.js
+++ controller/home.js
@@ -1,3 +1,25 @@
-module.exports = (req, res) => {
-  res.json({ version: '0.0.1' });
+const esSearch = require('../lib/esSearch');
+
+const EXCLUDE = require('../analysis/stop-words.json');
+
+module.exports = async (req, res) => {
+  const response = await esSearch({
+    mostTopic: {
+      terms: {
+        field: 'topic',
+        size: 100,
+        exclude: EXCLUDE,
+      },
+    },
+
+    mostText: {
+      terms: {
+        field: 'text',
+        size: 100,
+        exclude: EXCLUDE,
+      },
+    },
+  });
+
+  res.json(response);
 };
